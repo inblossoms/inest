01a87540c15c7590d8a131e5023b972a9b1f593d
inblossoms <L13994352192@163.com>
2025-05-24 12:32:32 +0800

    refactor: simplify exports in common package

--- src/packages/common/interfaces/modules/index.ts
+++ src/packages/common/interfaces/modules/index.ts
@@ -3,3 +3,4 @@ export * from "./injection-token.interface";
 export * from "./dynamic-module.interface";
 export * from "./module-metadata.interface";
 export * from "./provider.interface";
+export * from "./optionall-factory-dependency.interface";
