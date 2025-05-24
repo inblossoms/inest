fd64a595703ae08a9eb6e1ad9b4fe4c8cb448536
inblossoms <L13994352192@163.com>
2025-05-24 12:34:50 +0800

    fix: Improve the SetMetadata function to handle scenarios when the provider type is the object

--- src/packages/common/set-metadata.ts
+++ src/packages/common/set-metadata.ts
@@ -1,8 +1,13 @@
 import "reflect-metadata";
 import { MODULE_ISOLATION } from "./constants";
+import { isFunction } from "../shared/shared.utils";
 
 export function SetMetadata(module, metadata = []) {
-   metadata.forEach((metadata) => {
-      Reflect.defineMetadata(MODULE_ISOLATION, module, metadata);
+   metadata.forEach((item) => {
+      if (isFunction(item)) {
+         Reflect.defineMetadata(MODULE_ISOLATION, module, item);
+      } else if (item && typeof item === "object" && item.useClass) {
+         Reflect.defineMetadata(MODULE_ISOLATION, module, item.useClass);
+      }
    });
 }
